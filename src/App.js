e5cd41c401ede374fb9054a96c4a5b9b4c10853f
Anikesh-developer <anikesh.dsce98@gmail.com>
2023-02-12 19:16:36 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ const App = () => {
             <Routes>
               <Route path='https://Anikesh-developer.github.io/auth_login' element={<Header />}></Route>
             </Routes>
-            <Navbar />
-            <Header />
+            {/* <Navbar />
+            <Header /> */}
           </div>
           <Routes>
             <Route path='https://Anikesh-developer.github.io/auth_login' element={<Brand />}></Route>
@@ -54,13 +54,13 @@ const App = () => {
           <Routes>
             <Route path='https://Anikesh-developer.github.io/auth_login' element={<Footer />}></Route>
           </Routes>
-          <Brand />
+          {/* <Brand />
           <WhatGPT3 />
           <Features />
           <Possibility />
           <CTA />
           <Blog />
-          <Footer />
+          <Footer /> */}
         </div>
       </Router>
   )
