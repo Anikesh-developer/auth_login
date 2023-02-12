cba997ab82efd59fa6d45c269f6573fe3fd6e6a5
Anikesh-developer <anikesh.dsce98@gmail.com>
2023-02-12 20:01:32 +0530

    commit 18

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -74,7 +74,7 @@ const Login = () => {
                 await updateProfile(user , {
                     displayName: newEntry.email,
                 });
-                navigate('/auth_login');
+                navigate('/home');
                 setSubmitButtonDisabled(false);
             }).catch((err) => {  
                 setSubmitButtonDisabled(false);
