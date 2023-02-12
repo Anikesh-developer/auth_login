9b7679759a6dc6674554ae13211b38fb4747aa4f
Anikesh-developer <anikesh.dsce98@gmail.com>
2023-02-12 19:31:54 +0530

    updating changes

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ const App = () => {
         <div className="App">
           <div className='gradient__bg'>
             <Routes>
-              <Route path='https://anikesh-developer.github.io/' element={<Navbar name={userlogged} />}></Route>
+              <Route path='/auth_login' element={<Navbar name={userlogged} />}></Route>
             </Routes>
             <Routes>
-              <Route path='/' element={<Header />}></Route>
+              <Route path='/auth_login' element={<Header />}></Route>
             </Routes>
             {/* <Navbar />
             <Header /> */}
@@ -34,25 +34,25 @@ const App = () => {
             <Route path='/login' element={<Login />}></Route>
           </Routes>
           <Routes>
-            <Route path='/' element={<Brand />}></Route>
+            <Route path='/auth_login' element={<Brand />}></Route>
           </Routes>
           <Routes>
-            <Route path='/' element={<WhatGPT3 />}></Route>
+            <Route path='/auth_login' element={<WhatGPT3 />}></Route>
           </Routes>
           <Routes>
-            <Route path='/' element={<Features />}></Route>
+            <Route path='/auth_login' element={<Features />}></Route>
           </Routes>
           <Routes>
-            <Route path='/' element={<Possibility />}></Route>
+            <Route path='/auth_login' element={<Possibility />}></Route>
           </Routes>
           <Routes>
-            <Route path='/' element={<CTA />}></Route>
+            <Route path='/auth_login' element={<CTA />}></Route>
           </Routes>
           <Routes>
-            <Route path='/' element={<Blog />}></Route>
+            <Route path='/auth_login' element={<Blog />}></Route>
           </Routes>
           <Routes>
-            <Route path='/' element={<Footer />}></Route>
+            <Route path='/auth_login' element={<Footer />}></Route>
           </Routes>
           {/* <Brand />
           <WhatGPT3 />
